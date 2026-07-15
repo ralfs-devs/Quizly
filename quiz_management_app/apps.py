c81f435e68d00c653e2793c949c9d1ae9064262a
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-07-15 18:39:54 +0200

    Start Project incl. User Registration

---
+++ quiz_management_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuizManagementAppConfig(AppConfig):
+    name = 'quiz_management_app'
---
+++ user_auth_app/views.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from user_auth_app.serializers import RegistrationSerializer
+
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            data = {
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.pk
+            }
+            return Response(data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
